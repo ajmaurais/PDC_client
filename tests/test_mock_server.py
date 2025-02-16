28e95baf22fbce1b75e801adf0bd3b37b1bff189
Aaron Maurais <41207472+ajmaurais@users.noreply.github.com>
2025-02-15 19:17:55 -0800

    Delete stupid copilot artifact

--- tests/test_mock_server.py
+++ tests/test_mock_server.py
@@ -1,21 +1,3 @@
-def make_work_dir(work_dir, clear_dir=False)g
-    '''
-    Setup work directory for test.
-
-    Parameters
-    ----------
-    clear_dir: bool
-        If the directory already exists, should the files already in directory be deleted?
-        Will not work recursively or delete directories.
-    '''
-    if not os.path.isdir(work_dir):
-        if os.path.isfile(work_dir):
-            raise RuntimeError('Cannot create work directory!')
-        os.makedirs(work_dir)
-    else:
-        if clear_dir:
-            for file in os.listdir(work_dir):
-                os.remove(f'{work_dir}/{file}')
 
 import unittest
 import subprocess
@@ -25,8 +7,6 @@ import re
 import json
 import time
 import httpx
-
-# from requests.exceptions import ConnectionError
 
 import setup_tests
 
